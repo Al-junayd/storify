dedec609294566f4a0c4e3f22784d3da75eac155
Abdullah Aljunayd <laideopeyemi2013@gmail.com>
2024-09-29 23:42:37 +0100

    Adds Password Hashing logic

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -41,3 +42,28 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+const User = mongoose.model("User", userSchema);
+
+// Pre-save hook to hash the password before
+// saving the user to the database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to match the password entered by the user
+userSchema.methods.matchPasswords = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default User;
